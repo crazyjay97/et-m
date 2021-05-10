38f6b397fefb9a721e5a85a577cc625f4a8a3060
zhanweijie <zhanwj@zhiyuaniot.com>
2021-05-10 10:20:23 +0800

    init

---
+++ src/provider/wedding.ts
@@ -0,0 +1,42 @@
+import Dexie from 'dexie'
+
+
+const DB_NAME = 'WeddingDatabase'
+
+export default class WeddingDatabase extends Dexie {
+    wedding: Dexie.Table<IWedding, number>;
+    attachment: Dexie.Table<IAttachment, number>;
+
+    constructor() {
+        super(DB_NAME)
+
+        this.version(1).stores({
+            wedding: '++id,colorName,colorValue,desc,cost,price',
+            attachment: '++id,weddingId,name,path',
+        })
+
+        this.wedding = this.table('wedding')
+        this.attachment = this.table('attachment')
+    }
+}
+
+
+export interface IWedding extends IPrice {
+    id?: number
+    colorName: string
+    colorValue: string
+    desc: string
+}
+
+export interface IAttachment {
+    id?: number
+    weddingId: number
+    name: string
+    path: string
+}
+
+
+interface IPrice {
+    cost: number
+    price: number
+}
---
+++ src/views/wedding-case/index.vue
@@ -0,0 +1,228 @@
+<template>
+    <div class="wedding-wrap">
+        <el-button type="text" @click="add" class="add-btn" icon="el-icon-plus"></el-button>
+        <div class="color-card-wrap">
+            <div v-for="item in dataList.list" class="color-card" :style="{background:`linear-gradient(180deg,${item.colorValue},85%,#FFB6C1)`}">
+                {{ item.colorName }}
+                <div>
+                    {{ item.desc }}
+                </div>
+            </div>
+        </div>
+        <el-drawer
+                title="添加"
+                v-model="showAdd"
+                :before-close="handleClose"
+                destroy-on-close>
+            <el-form ref="saveFormEl" :model="formData" label-position="top">
+                <el-row justify="20">
+                    <el-col :span="24">
+                        <el-form-item prop="colorName" label="名称" :rules="[
+                                    {
+                                        required: true,
+                                        message: '必填项',
+                                        trigger: 'change'
+                                    }
+                                ]">
+                            <el-input v-model="formData.colorName"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="colorValue" label="颜色(16进制)" :rules="[
+                                    {
+                                        required: true,
+                                        message: '必填项',
+                                        trigger: 'change'
+                                    }
+                                ]">
+                            <el-input v-model="formData.colorValue"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="cost" label="成本" :rules="[
+                                    {
+                                        required: true,
+                                        message: '必填项',
+                                        trigger: 'change'
+                                    }
+                                ]">
+                            <el-input v-model="formData.cost"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="price" label="价格" :rules="[
+                                    {
+                                        required: true,
+                                        message: '必填项',
+                                        trigger: 'change'
+                                    }
+                                ]">
+                            <el-input v-model="formData.price"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="desc" label="描述">
+                            <el-input type="textarea" v-model="formData.desc"
+                                      :autosize="{ minRows: 4, maxRows: 8}"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="attachment" label="附件">
+                            <el-upload
+                                    ref="uploadEl"
+                                    class="upload-demo"
+                                    action="xx"
+                                    :on-remove="handleRemove"
+                                    :before-remove="beforeRemove"
+                                    multiple
+                                    :http-request="saveFile"
+                                    :file-list="formData.attachment"
+                            >
+                                <el-button size="small" type="primary">点击上传</el-button>
+                                <template #tip>
+                                    <div class="el-upload__tip">只能选择 jpg/png/mp4 文件</div>
+                                </template>
+                            </el-upload>
+
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-button type="primary" style="width: 100%" @click="saveForm">保存</el-button>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </el-drawer>
+
+    </div>
+
+</template>
+<script lang="ts">
+import './index.sass'
+import {defineComponent, onMounted, reactive, ref} from 'vue'
+import WeddingProvider from '@/provider/wedding'
+import WeddingDatabase, {IWedding} from '@/provider/wedding'
+import {ElMessage, ElMessageBox} from 'element-plus'
+import {uuid} from "@/utils/utils";
+
+const fs = window.require('fs')
+
+
+    interface FormModel {
+        colorName: string
+        colorValue: string
+        desc: string
+        price: number
+        cost: number
+        attachment: FileInfo []
+    }
+
+    interface FileInfo {
+        id?: number
+        name: string
+        url: string
+    }
+
+    export default defineComponent({
+        setup() {
+            const db = new WeddingProvider()
+            const dataList = reactive({
+                list: [] as IWedding[]
+            })
+            const saveFormEl = ref()
+            const uploadEl = ref()
+            const formData = reactive({}) as FormModel
+            const showAdd = ref(false)
+            const loadDate = () => {
+                db.wedding.toCollection().toArray().then((weddingList: IWedding[]) => {
+                    dataList.list = weddingList
+                })
+            }
+            onMounted(() => {
+                showAdd.value = false
+                loadDate()
+            })
+            const add = () => {
+                showAdd.value = true
+            }
+            const handleClose = () => {
+                showAdd.value = false
+                saveFormEl.value.resetFields({})
+                uploadEl.value.clearFiles()
+            }
+            const handleRemove = (file: any, fileList: any[]) => {
+                console.log(file, fileList)
+            }
+            const beforeRemove = (file: any, fileList: any[]) => {
+                return ElMessageBox.confirm(`确定移除 ${file.name}？`);
+            }
+            const saveFile = (r: any) => {
+                const path = './' + uuid() + r.file.name
+                fs.writeFileSync(path, fs.readFileSync(r.file.path));
+                formData.attachment = [...(formData.attachment ? formData.attachment : []), {
+                    name: r.file.name,
+                    url: path
+                }]
+            }
+            const saveForm = () => {
+                saveFormEl.value.validate((valid: boolean) => {
+                    if (valid) {
+                        const db = new WeddingDatabase()
+                        db.wedding.put({
+                            colorName: formData.colorName,
+                            colorValue: formData.colorValue,
+                            desc: formData.desc,
+                            cost: formData.cost,
+                            price: formData.price,
+                        }).then(id => {
+                            if (formData.attachment) {
+                                const ps: Promise<any>[] = []
+                                formData.attachment.forEach(r => {
+                                    if (!r.id) {
+                                        ps.push(db.attachment.put({
+                                            weddingId: id,
+                                            name: r.name,
+                                            path: r.url
+                                        }))
+                                    }
+                                })
+                                Promise.all(ps).then(() => {
+                                    ElMessage({
+                                        type: 'success',
+                                        message: '保存成功',
+                                        duration: 2000,
+                                        onClose: () => {
+                                            showAdd.value = false
+                                        }
+                                    })
+                                })
+                            } else {
+                                ElMessage({
+                                    type: 'success',
+                                    message: '保存成功',
+                                    duration: 2000,
+                                    onClose: () => {
+                                        showAdd.value = false
+                                        loadDate()
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+            return {
+                add,
+                showAdd,
+                dataList,
+                handleClose,
+                formData,
+                handleRemove,
+                beforeRemove,
+                saveFile,
+                saveFormEl,
+                saveForm,
+                uploadEl
+            }
+        }
+    })
+</script>
